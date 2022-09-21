ec4cfdbdbec82e2b53765e817d82439418be9a00
NIKHILKEDAR8805 <112891692+NIKHILKEDAR8805@users.noreply.github.com>
2022-09-22 00:33:35 +0530

    assignment02
    question02

---
+++ assignment02_question02.py
@@ -0,0 +1,13 @@
+import string
+
+alphabet_list = list(string.ascii_lowercase)
+
+lst1 =list(alphabet_list)
+lst2 = []
+for i in lst1:
+    ascii = ord(i)
+    lst2.append(ascii)
+# print(lst1)
+# print(lst2)
+dict1 =dict(zip(lst1,lst2))
+print(dict1)
