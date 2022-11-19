68bc02a4b327c1f4afc457ccc8e47dfc9aeaf9e1
NIKHILKEDAR8805 <112891692+NIKHILKEDAR8805@users.noreply.github.com>
2022-11-19 15:43:40 +0530

    Update assignment02_question02.py

--- assignment02_question02.py
+++ assignment02_question02.py
@@ -2,7 +2,7 @@ import string
 
 alphabet_list = list(string.ascii_lowercase)
 
-lst1 =list(alphabet_list)
+lst1 =alphabet_list
 lst2 = []
 for i in lst1:
     ascii = ord(i)
@@ -10,4 +10,4 @@ for i in lst1:
 # print(lst1)
 # print(lst2)
 dict1 =dict(zip(lst1,lst2))
-print(dict1)
+print("output :\n",dict1)
